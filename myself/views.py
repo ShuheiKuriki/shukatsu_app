15f858f0ab7426d1b0051b3996819626699f3396
shuhei <shukuri.7336.8@gmail.com>
2020-03-16 06:07:06 +0900

    その他

--- myself/views.py
+++ myself/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'menu/myself.html'
+    template_name = 'myself/myself.html'
 
